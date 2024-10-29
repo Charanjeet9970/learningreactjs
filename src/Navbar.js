0e784c71f4a618c65f431474b00acd1fb977eb54
Charanjeet <sachdevcharan903@gmail.com>
2024-10-29 13:59:18 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <h1>The Blogs</h1>
+            <div className="links">
+                <a href="./">Home</a>
+                <a href=".create"></a> 
+            </div>
+        </div>
+    );
+}
+ 
+export default Navbar;
